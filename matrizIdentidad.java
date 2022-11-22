af3c33f9d3cbd79dea04789ea2ef46cbd1ce4b87
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-22 12:29:19 +0100

    ejercicio planteado en clase

---
+++ matrizIdentidad.java
@@ -0,0 +1,23 @@
+
+public class matrizIdentidad{
+	public static void main(String[] args){
+        int[][] matriz=new int[5][5];
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(j==i){
+                    matriz[i][j]=1;
+                }else{
+                    matriz[i][j]=0;
+                }
+            }
+        } 
+ 
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
