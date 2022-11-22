2421c8107d93ba1856282540c7c16242f7a64743
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-22 12:48:02 +0100

    escaleras con matrices

---
+++ escaleras.java
@@ -0,0 +1,31 @@
+/*Crear unas escaleras con una matriz
+ * 
+ * @author Antonio Luis Garcia
+ */
+import java.util.Scanner;
+public class escaleras{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Diga la anchura de la matriz");
+        int ancho=sc.nextInt();
+        int[][] matriz=new int[ancho][ancho];
+        for(int i=0;i<ancho;i++){
+            for(int j=0;j<ancho;j++){
+                if(j<=i){
+                    matriz[i][j]=1;
+                }else{     
+                        matriz[i][j]=0;
+                }
+            }
+        } 
+ 
+        for(int i=0;i<ancho;i++){
+            for(int j=0;j<ancho;j++){
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
