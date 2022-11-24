22f7cf7101f42a83622456615add3da84feed4a9
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-24 08:52:49 +0100

    ejercicio 7 corregido

--- Ejercicio07.java
+++ Ejercicio07.java
@@ -14,31 +14,23 @@ public class Ejercicio07{
         int[] array= new int[100];
         for(int i=0;i<100;i++){
             array[i]=(int)(Math.random()*21);
-        }
-
-        for(int i=0;i<100;i++){
             System.out.print(array[i]+" ");
         }
-
-        int aleatorio1=(int)(Math.random()*101);
-        int aleatorio2;//estos numeros para cambiar una posicion aleatoria del array
-
-        do{
-            aleatorio2=(int)(Math.random()*101);
-        }while(aleatorio2==aleatorio1);
 
         //el bucle para que no sean los mismos numeros aunque sea poco probable coincidir
 
         System.out.println("");
         System.out.println("Ahora cambiaras dos numeros del array aleatorios");
-        array[aleatorio1]=sc.nextInt();
-        array[aleatorio2]=sc.nextInt();
+        int buscar=sc.nextInt();
+        System.out.println("Ahora cambiaras dos numeros del array aleatorios");
+        int sustituto=sc.nextInt();
 
         for(int i=0;i<100;i++){
-            if(i==aleatorio1 || i==aleatorio2){
-                System.out.print(array[i]+" ");
+            if(array[i]==buscar){
+                array[i]=sustituto;
+                System.out.print("\"%d\" "+array[i]+" ");
             }else{
-                System.out.print("\""+array[i]+"\" ");
+                System.out.print(" "+array[i]+" ");
             }
         }
 
